47a44fc4875d1328b9167e3abbb2a47fe7f49e2d
Divyansh <divyanshraj1@gmai.com>
2025-02-04 02:11:31 +0530

    commit(validation)

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 from fastapi.param_functions import Body
+from pydantic import BaseModel
+
+
+
+
 app = FastAPI()
+
+class post(BaseModel):      #pydantic to validate the JSON payload from RAW body yady
+    title: str
+    content: str
 
 
 @app.get("/")
@@ -12,6 +21,8 @@ def get_posts():
     return {"data": "post"}
 
 @app.post("/createposts")
-def create_posts(payLoad: dict = Body(...)):
-    print(payLoad)
-    return {"new_post": f"title {payLoad['title']} content: {payLoad['content']}"}
+def create_posts(new_post: post): #taken from the base class
+    print(new_post.title)   #just the title for content do new_post.content
+    return {"data": "post"}                    # unvalidated request {"new_post": f"title {payLoad['title']} content: {payLoad['content']}"}
+
+#validating title str, content str
