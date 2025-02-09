8a0cfa0d93451f10c2f3312895973498911ce439
Divyansh <divyanshraj1@gmai.com>
2025-02-09 21:28:04 +0530

    memory

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.param_functions import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 
 
 
@@ -14,6 +15,8 @@ class post(BaseModel):      #pydantic to validate the JSON payload from RAW body
     published: bool = True # optional values
     rating: Optional[int] = None    
 
+my_posts = [{'title': 'title of post 1', 'content': 'content of post1', 'id': 1}]
+
 
 @app.get("/")
 async def read_root():
@@ -24,11 +27,11 @@ def get_posts():
     return {"data": "post"}
 
 @app.post("/createposts")
-def create_posts(new_post: post):                            #taken from the base class
-    print(new_post)
-    print(new_post.model_dump())           #a different form of dict?????
-
-                                               
-    return {"data": new_post}                    # unvalidated request {"new_post": f"title {payLoad['title']} content: {payLoad['content']}"}
+def create_posts(posts: post):
+    post_dict = posts.dict()
+    post_dict['id'] = randrange(0, 1000)
+    my_posts.append(post_dict)
+                                                                        
+    return {"data": post_dict}                    # unvalidated request {"new_post": f"title {payLoad['title']} content: {payLoad['content']}"}
 
 #validating title str, content str
